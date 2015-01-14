6cd86d402aefccd5be2a36d4cd8989857adfbf09
anthony-ng <thomsonng123@gmail.com>
2015-01-13 21:25:07 -0800

    updated

--- app/controllers/article.rb
+++ app/controllers/article.rb
@@ -13,11 +13,6 @@ end
 
 post '/categories/:category_id/articles' do
   # @category = Category.find(params[:category_id])
-  p "hello"
-  p  @category
-
-  # puts "this is category:"
-  # p category
   article = Article.create(params[:article])
   article.update_attributes(category_id: 1)
   redirect "/categories/1/articles"
