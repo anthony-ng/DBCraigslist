600694211791469fdd2e80f1a97b2804fd0be37e
Anthony Ng & cmliotta <pair+anthony-ng.cmliotta@devbootcamp.com>
2015-01-13 17:06:46 -0800

    updated all 7 nested routes

--- app/controllers/article.rb
+++ app/controllers/article.rb
@@ -2,7 +2,7 @@ get '/categories/:category_id/articles' do
   @category = Category.find(params[:category_id])
   @articles = @category.articles
 
-  erb :"articles/articles"
+  erb :"articles/show"
 end
 
 get '/categories/:category_id/articles/new' do
@@ -12,22 +12,46 @@ get '/categories/:category_id/articles/new' do
 end
 
 post '/categories/:category_id/articles' do
-  # @category = Category.find(params[:category_id])
-  p "hello"
-  p  @category
+  id = params[:category_id]
 
-  # puts "this is category:"
-  # p category
   article = Article.create(params[:article])
-  article.update_attributes(category_id: 1)
-  redirect "/categories/1/articles"
+
+  article.category = Category.find(id)
+  article.save
+
+  redirect "/categories/#{id}/articles"
 end
 
 get '/categories/:category_id/articles/:article_id' do
   @category = Category.find(params[:category_id])
   @article = @category.articles.find(params[:article_id])
 
-  erb :"articles/articles"
+  erb :"articles/show"
+end
+
+get '/categories/:category_id/articles/:article_id/edit' do
+  @category = Category.find(params[:category_id])
+  @article = @category.articles.find(params[:article_id])
+
+  erb :"articles/edit"
+end
+
+put '/categories/:category_id/articles/:article_id' do
+  id = params[:category_id]
+
+  article = Article.find(params[:article_id])
+  article.update_attributes(params[:article])
+
+  redirect "/categories/#{id}/articles"
+end
+
+delete '/categories/:category_id/articles/:article_id' do
+  id = params[:category_id]
+
+  article = Article.find(params[:article_id])
+  article.destroy
+
+  redirect "/categories/#{id}/articles"
 end
 
 
