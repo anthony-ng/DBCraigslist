0a7e55bb53351a011e2baec1a2033935a1ed6efc
Anthony Ng & cmliotta <pair+anthony-ng.cmliotta@devbootcamp.com>
2015-01-13 19:30:24 -0800

    DRY IT UP using before

--- app/controllers/article.rb
+++ app/controllers/article.rb
@@ -1,40 +1,44 @@
+before %r[/categories/(\d+)] do |category_id|
+  @category = Category.find(category_id)
+end
+
+before %r[/categories/(\d+)/articles/(\d+)] do |category_id, article_id|
+  @article = @category.articles.find(article_id)
+end
+
 get '/categories/:category_id/articles' do
-  @category = Category.find(params[:category_id])
+  # @category = Category.find(params[:category_id])
   @articles = @category.articles
 
   erb :"articles/show"
 end
 
 get '/categories/:category_id/articles/new' do
-  @category = Category.find(params[:category_id])
+  # @category = Category.find(params[:category_id])
 
   erb :"articles/new"
 end
 
 post '/categories/:category_id/articles' do
-  @category = Category.find(params[:category_id])
-
+  # @category = Category.find(params[:category_id])
   @article = Article.create(params[:article])
 
   @article.category = @category
   @article.save
 
   erb :"articles/success"
-
-  # "/categories/#{id}/articles"
 end
 
 get '/categories/:category_id/articles/:article_id' do
-  @category = Category.find(params[:category_id])
-  @article = @category.articles.find(params[:article_id])
+  # @category = Category.find(params[:category_id])
+  # @article = @category.articles.find(params[:article_id])
 
   erb :"articles/show"
 end
 
 get '/categories/:category_id/articles/:article_id/edit' do
-
-  @category = Category.find(params[:category_id])
-  @article = @category.articles.find(params[:article_id])
+  # @category = Category.find(params[:category_id])
+  # @article = @category.articles.find(params[:article_id])
 
   if params[:key] == @article.article_key
     erb :"articles/edit"
@@ -44,19 +48,23 @@ get '/categories/:category_id/articles/:article_id/edit' do
 end
 
 put '/categories/:category_id/articles/:article_id' do
-  id = params[:category_id]
+  # @category = Category.find(params[:category_id])
+  # @article = @category.articles.find(params[:article_id])
 
-  article = Article.find(params[:article_id])
   article.update_attributes(params[:article])
+
+  id = params[:category_id]
 
   redirect "/categories/#{id}/articles"
 end
 
 delete '/categories/:category_id/articles/:article_id' do
-  id = params[:category_id]
+  # @category = Category.find(params[:category_id])
+  # @article = @category.articles.find(params[:article_id])
 
-  article = Article.find(params[:article_id])
   article.destroy
+
+  id = params[:category_id]
 
   redirect "/categories/#{id}/articles"
 end
